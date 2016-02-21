8e14f0223eebe40a354e554ba9bcb4773a817aa2
Rögnvaldur Jóhann Sæmundsson <rjs@rognvaldurs-macbook-pro.local>
2016-02-21 08:47:44 +0100

    Adapted ViewController to use the WeatherData class. Fixed bugs in WeatherData class.

--- WeatherPredictor/WeatherData.swift
+++ WeatherPredictor/WeatherData.swift
@@ -95,46 +95,55 @@ class Temperature {
 }
 
 class WeatherData: NSObject {
-    var parseData : [PFObject] = []
-    var startDate : NSDate
-    var stopDate : NSDate
+    var mParseData : [PFObject] = []
+    var mStartDate : NSDate
+    var mStopDate : NSDate
+    var mTableView : UITableView!
+    var mNumDaysBackAndForward : Int
     
     override init() {
-        startDate = NSDate()
-        stopDate = NSDate()
+        mStartDate = NSDate()
+        mStopDate = NSDate()
+        mNumDaysBackAndForward = 6
     }
     
-    func fetchParseData(focusDate : NSDate) {
+    func fetchParseData(focusDate : NSDate, tableView : UITableView) {
         //  Fetch records centered around the focus date. Historical records backward and predictions forward
         //  Predictions are all found in the records of the focusdate.
         
-        let numDaysBack = 6
+        mTableView = tableView
+        
         
         // Create the start and stop dates for the query
         
         let calendar = NSCalendar.currentCalendar()
-        var components = calendar.components([.Year, .Month, .Day, .Hour, .Minute], fromDate: calendar.dateByAddingUnit(.Day, value: -numDaysBack, toDate: focusDate, options: NSCalendarOptions(rawValue:0))!)
+        var components = calendar.components([.Year, .Month, .Day, .Hour, .Minute], fromDate: calendar.dateByAddingUnit(.Day, value: -mNumDaysBackAndForward, toDate: focusDate, options: NSCalendarOptions(rawValue:0))!)
         components.hour = 0
         components.minute = 0
-        self.startDate = calendar.dateFromComponents(components)!
+        self.mStartDate = calendar.dateFromComponents(components)!
         
         components = calendar.components([.Year, .Month, .Day, .Hour, .Minute], fromDate: calendar.dateByAddingUnit(.Day, value: 1, toDate: focusDate, options: NSCalendarOptions(rawValue:0))!)
         components.hour = 0
         components.minute = 0
-        self.stopDate = calendar.dateFromComponents(components)!
+        self.mStopDate = calendar.dateFromComponents(components)!
         
         // Build the query
         
         let query = PFQuery(className:"Data")
-        query.whereKey("date", greaterThan: startDate)
-        query.whereKey("date", lessThan: stopDate)
+        query.whereKey("date", greaterThan: mStartDate)
+        query.whereKey("date", lessThan: mStopDate)
+        query.orderByAscending("date")
+        
+        mStopDate = focusDate   // Reset mStopDate to focusDate because we had added one day to much
         
         // Run the query and calculate the average for each day
         // Each timestamp should hold current value and seven days forward prediction.
         
         query.findObjectsInBackgroundWithBlock { (objects, error) -> Void in
             if error == nil {
-                self.parseData = objects!
+                self.mParseData = objects!
+                self.mTableView.reloadData()
+                print("Gögnin komin")
             } else {
                 print(error)
             }
@@ -145,39 +154,86 @@ class WeatherData: NSObject {
     func getAverageTemperatures(focusDate : NSDate) -> [Temperature] {
         
         // Fetch data from server if not already there
-        if (focusDate.isEqualDate(self.stopDate) == false) {
-            fetchParseData(focusDate)
+        if (focusDate.isEqualDay(self.mStopDate) == false) {
+            fetchParseData(focusDate,tableView: mTableView)
         }
         
-        var currentDate = startDate
+        var currentDate = mStartDate
         var temperatures : [Temperature] = []
         var countMeasurements = 0
+        var predictions : [Temperature] = []
         
-        temperatures[0].value = 0   // Add the first value to the array
+        temperatures.append(Temperature())   // Add the first value to the array to return
         
-        for object in parseData {
+        for _ in 0...mNumDaysBackAndForward {
+            predictions.append(Temperature())
+        }
+        
+        print("Lengd mParseData: \(mParseData.count)")
+        
+        for object in mParseData {
+            print("Núverandi dagsetning: \(object["date"] as! NSDate)")
+            print("Núverandi hitastig: \(object["current"] as! Int)")
             if (object["date"] as! NSDate).isEqualDay(currentDate){
-                // If measurements are from the same date they are averaged
+                //  If measurements are from the same date they are averaged
                 //  First added together and then divided by the count once all are found
+                //  The focusdate is special because it has the predictions for six days ahead
+               
                 temperatures[temperatures.count-1].value += object["current"] as! Double
                 countMeasurements++
+                
+                // NOTE: number of predictions is hardcoded here
+                if (object["date"] as! NSDate).isEqualDay(focusDate){
+                    predictions[0].value += object["prediction1"] as! Double
+                    predictions[1].value += object["prediction2"] as! Double
+                    predictions[2].value += object["prediction3"] as! Double
+                    predictions[3].value += object["prediction4"] as! Double
+                    predictions[4].value += object["prediction5"] as! Double
+                    predictions[5].value += object["prediction6"] as! Double
+                }
             } else {
                 // All measurements have been found and we divide by the count to find average
                 // and add the date
                 temperatures[temperatures.count-1].value /= Double(countMeasurements)
                 temperatures[temperatures.count-1].date = currentDate
-                
-                // Reset counter and add next value to the array
+                print("Meðaltal \(currentDate) : \(temperatures[temperatures.count-1].value)")
                 countMeasurements = 0
                 
                 // Reset currentDate with the new date and add the first value
                 currentDate = object["date"] as! NSDate
-                temperatures[temperatures.count].value = 0    // count will increase by one
+                temperatures.append(Temperature())    // count will increase by one
                 temperatures[temperatures.count-1].value += object["current"] as! Double
                 countMeasurements++
             }
             
         }
+        
+        // Calculate the mean and set the date for the last date (which is focusDate) as well as the predictions
+        temperatures[temperatures.count-1].value /= Double(countMeasurements)
+        temperatures[temperatures.count-1].date = currentDate
+        
+        // NOTE: number of predictions is hardcoded here
+        predictions[0].value /= Double(countMeasurements)
+        predictions[0].date = currentDate.addDays(1)
+        predictions[1].value /= Double(countMeasurements)
+        predictions[1].date = predictions[0].date.addDays(1)
+        predictions[2].value /= Double(countMeasurements)
+        predictions[2].date = predictions[1].date.addDays(1)
+        predictions[3].value /= Double(countMeasurements)
+        predictions[3].date = predictions[2].date.addDays(1)
+        predictions[4].value /= Double(countMeasurements)
+        predictions[4].date = predictions[3].date.addDays(1)
+        predictions[5].value /= Double(countMeasurements)
+        predictions[5].date = predictions[4].date.addDays(1)
+        
+        // Append the predictions to the list
+        temperatures.append(predictions[0])
+        temperatures.append(predictions[1])
+        temperatures.append(predictions[2])
+        temperatures.append(predictions[3])
+        temperatures.append(predictions[4])
+        temperatures.append(predictions[5])
+        
         return temperatures
     }
     
@@ -185,7 +241,9 @@ class WeatherData: NSObject {
         var temperatures : [Temperature] = []
         var found : Bool = false
         
-        for object in parseData {
+        // Assumes date is in mParseData. If not nothing is returned.
+        
+        for object in mParseData {
             if (object["date"] as! NSDate).isEqualDay(date){
                 temperatures.append(Temperature(aDate: object["date"] as! NSDate, aValue: object["current"] as! Double))
                 found = true
--- WeatherViewController.swift
+++ WeatherViewController.swift
@@ -9,162 +9,20 @@
 import UIKit
 
 class WeatherViewController: UITableViewController {
-    var weatherData : WeatherData
+    var mWeatherData     : WeatherData!
+    var mFocusDate       : NSDate!
+    var mTemperatures    : [Temperature] = []
     
-    
-/*    func fetchParseData(focusDate : NSDate) {
-        //  Fetch records centered around the focus date. Historical records backward and predictions forward
-        //  Predictions are all found in the records of the focusdate.
-        
-        let numDaysBack = 6
-        
-        // Create the start and stop dates for the query
-        
-        let calendar = NSCalendar.currentCalendar()
-        var components = calendar.components([.Year, .Month, .Day, .Hour, .Minute], fromDate: calendar.dateByAddingUnit(.Day, value: -numDaysBack, toDate: focusDate, options: NSCalendarOptions(rawValue:0))!)
-        components.hour = 0
-        components.minute = 0
-        let queryStartDate = calendar.dateFromComponents(components)!
-        
-        components = calendar.components([.Year, .Month, .Day, .Hour, .Minute], fromDate: calendar.dateByAddingUnit(.Day, value: 1, toDate: focusDate, options: NSCalendarOptions(rawValue:0))!)
-        components.hour = 0
-        components.minute = 0
-        let queryStopDate = calendar.dateFromComponents(components)!
-        
-        // Build the query
-        
-        let query = PFQuery(className:"Data")
-        query.whereKey("date", greaterThan: queryStartDate)
-        query.whereKey("date", lessThan: queryStopDate)
-        
-        // Run the query and calculate the average for each day
-        // Each timestamp should hold current value and seven days forward prediction.
-        
-        query.findObjectsInBackgroundWithBlock { (objects, error) -> Void in
-            if error == nil {
-                print(objects)
-  /*              var currentDate
-                var totalTemp = 0
-                for object in objects! {
-                    //totalTemp += object["current"] as! Int
-                    //print(object)
-                    
-                }
-                print(self.averageTemp(objects!))*/
-                //let avTemp = totalTemp/objects!.count
-                
-            } else {
-                print(error)
-            }
-            self.tableView.reloadData()
-            
-            let indexPath = NSIndexPath(forRow: 12, inSection: 0)
-            self.tableView.scrollToRowAtIndexPath(indexPath, atScrollPosition: UITableViewScrollPosition.Bottom, animated: false)
-        }
-        
-    }
-    
-    func fetchParseData() {
-        let dateFormatter = NSDateFormatter()
-        dateFormatter.dateFormat = "dd.MM.yyyy HH:mm"
-        let today = dateFormatter.dateFromString("02.12.2015 00:00")
-        
-        let calendar = NSCalendar.currentCalendar()
-        var components = calendar.components([.Year, .Month, .Day, .Hour, .Minute], fromDate: calendar.dateByAddingUnit(.Day, value: -6, toDate: today!, options: NSCalendarOptions(rawValue:0))!)
-        components.hour = 0
-        components.minute = 0
-        let searchStart = calendar.dateFromComponents(components)!
-        
-        components = calendar.components([.Year, .Month, .Day, .Hour, .Minute], fromDate: calendar.dateByAddingUnit(.Day, value: 6, toDate: today!, options: NSCalendarOptions(rawValue:0))!)
-        components.hour = 23
-        components.minute = 59
-        let searchStop = calendar.dateFromComponents(components)!
-        
-        let query = PFQuery(className:"Data")
-        query.whereKey("date", greaterThan: searchStart)
-        query.whereKey("date", lessThan: searchStop)
-        query.findObjectsInBackgroundWithBlock { (objects, error) -> Void in
-            if error == nil {
-                var currentDate = searchStart
-                var rawTemps : [String : [Int]] = [:]
-                
-                dateFormatter.dateFormat = "dd.MM.yyyy"
-                
-                for object in objects! {
-                    if object["date"].isEqualToDate(today!) {
-                        // Todays date
-                        rawTemps[dateFormatter.stringFromDate(currentDate)]! += [object["current"] as! Int]
-                        
-                        // Future dates
-                        
-                        // Adding one day to currentDate
-                        components = calendar.components([.Year, .Month, .Day], fromDate: calendar.dateByAddingUnit(.Day, value: 1, toDate: currentDate, options: NSCalendarOptions(rawValue:0))!)
-                        // Setting value for prediction for day 1
-                        rawTemps[dateFormatter.stringFromDate(calendar.dateFromComponents(components)!)]! += [object["prediction1"] as! Int]
-                        
-                        // Adding two days to currentDate
-                        components = calendar.components([.Year, .Month, .Day], fromDate: calendar.dateByAddingUnit(.Day, value: 2, toDate: currentDate, options: NSCalendarOptions(rawValue:0))!)
-                        // Setting value for prediction for day 2
-                        rawTemps[dateFormatter.stringFromDate(calendar.dateFromComponents(components)!)]! += [object["prediction2"] as! Int]
-                        
-                        // Adding three days to currentDate
-                        components = calendar.components([.Year, .Month, .Day], fromDate: calendar.dateByAddingUnit(.Day, value: 3, toDate: currentDate, options: NSCalendarOptions(rawValue:0))!)
-                        // Setting value for prediction for day 3
-                        rawTemps[dateFormatter.stringFromDate(calendar.dateFromComponents(components)!)]! += [object["prediction3"] as! Int]
-                        
-                        // Adding four days to currentDate
-                        components = calendar.components([.Year, .Month, .Day], fromDate: calendar.dateByAddingUnit(.Day, value: 4, toDate: currentDate, options: NSCalendarOptions(rawValue:0))!)
-                        // Setting value for prediction for day 4
-                        rawTemps[dateFormatter.stringFromDate(calendar.dateFromComponents(components)!)]! += [object["prediction4"] as! Int]
-                        
-                        // Adding five days to currentDate
-                        components = calendar.components([.Year, .Month, .Day], fromDate: calendar.dateByAddingUnit(.Day, value: 5, toDate: currentDate, options: NSCalendarOptions(rawValue:0))!)
-                        // Setting value for prediction for day 5
-                        rawTemps[dateFormatter.stringFromDate(calendar.dateFromComponents(components)!)]! += [object["prediction5"] as! Int]
-                        
-                        // Adding six days to currentDate
-                        components = calendar.components([.Year, .Month, .Day], fromDate: calendar.dateByAddingUnit(.Day, value: 6, toDate: currentDate, options: NSCalendarOptions(rawValue:0))!)
-                        // Setting value for prediction for day 6
-                        rawTemps[dateFormatter.stringFromDate(calendar.dateFromComponents(components)!)]! += [object["prediction6"] as! Int]
-                    }
-                    else if object["date"].isEqualToDate(currentDate) {
-                        // Todays date
-                        rawTemps[dateFormatter.stringFromDate(currentDate)]! += [object["current"] as! Int]
-                    } else {
-                        components = calendar.components([.Year, .Month, .Day], fromDate: calendar.dateByAddingUnit(.Day, value: 1, toDate: currentDate, options: NSCalendarOptions(rawValue:0))!)
-                        currentDate = calendar.dateFromComponents(components)!
-                    }
-                }
-        
-                //let avTemp = totalTemp/objects!.count
-                self.predictions += [objects![0], objects![4], objects![8], objects![12], objects![16], objects![20], objects![24], objects![28], objects![32], objects![36], objects![40], objects![44], objects![48]]
-                self.tableView.reloadData()
-            } else {
-                print(error)
-            }
-            
-            let indexPath = NSIndexPath(forRow: 12, inSection: 0)
-            self.tableView.scrollToRowAtIndexPath(indexPath, atScrollPosition: UITableViewScrollPosition.Bottom, animated: false)
-        }
-        
-    }
-    */
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let dateFormatter = NSDateFormatter()
         dateFormatter.dateFormat = "dd.MM.yyyy HH:mm"
-        let today = dateFormatter.dateFromString("02.12.2015 00:00")
+        mFocusDate = dateFormatter.dateFromString("02.12.2015 00:00")!
 
-        weatherData = WeatherData()
+        mWeatherData = WeatherData()
         
-        weatherData.fetchParseData(today!)
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        mWeatherData.fetchParseData(mFocusDate,tableView: self.tableView)
     }
 
     override func didReceiveMemoryWarning() {
@@ -175,13 +33,13 @@ class WeatherViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 13
+        mTemperatures = mWeatherData.getAverageTemperatures(mFocusDate)
+        
+        return mTemperatures.count
     }
     
     override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
@@ -199,14 +57,14 @@ class WeatherViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var returnCell : UITableViewCell = UITableViewCell()
         
-        if predictions.count > 0 {
+        if mTemperatures.count > 0 {
             let dateFormatter = NSDateFormatter()
             dateFormatter.dateFormat = "dd.MM.yyyy"
             dateFormatter.locale = NSLocale(localeIdentifier: "is_IS")
             let today = dateFormatter.dateFromString("02.12.2015")
             
-            var predDate = predictions[indexPath.row]["date"] as! NSDate
-            let predTemp = predictions[indexPath.row]["current"] as! Int
+            var predDate = mTemperatures[indexPath.row].date
+            let predTemp = mTemperatures[indexPath.row].value
             
             let calendar = NSCalendar.currentCalendar()
             let components = calendar.components([.Year, .Month, .Day, .Hour, .Minute], fromDate: calendar.dateByAddingUnit(.Day, value: 0, toDate: predDate, options: NSCalendarOptions(rawValue:0))!)
@@ -218,8 +76,6 @@ class WeatherViewController: UITableViewController {
         
             dateFormatter.dateFormat = "EEEE"
             let weekDay = dateFormatter.stringFromDate(predDate)
-            
-            print(weekDay)
             
             if predDate.isEqualToDate(today!) { //This will be changed to NSDate.date() in the future
                 let cell = tableView.dequeueReusableCellWithIdentifier("TodayCell", forIndexPath: indexPath) as! TodayCell
@@ -252,53 +108,6 @@ class WeatherViewController: UITableViewController {
 
             }
         }
-        
-        /*if temps.count > 0 {
-            let dateFormatter = NSDateFormatter()
-            dateFormatter.dateFormat = "dd.MM.yyyy"
-            dateFormatter.locale = NSLocale(localeIdentifier: "is_IS")
-            let today = dateFormatter.dateFromString("02.12.2015")
-            
-            let predDate = dateFormatter.dateFromString(Array(temps.keys)[indexPath.row])
-            let predTemp = temps[Array(temps.keys)[indexPath.row]]  
-            
-            dateFormatter.dateFormat = "EEEE"
-            let weekDay = dateFormatter.stringFromDate(predDate)
-            
-            print(weekDay)
-            
-            if predDate.isEqualToDate(today!) { //This will be changed to NSDate.date() in the future
-                let cell = tableView.dequeueReusableCellWithIdentifier("TodayCell", forIndexPath: indexPath) as! TodayCell
-                
-                if predTemp > 0 {
-                    cell.tempLabel.textColor = UIColor(red: 255/255, green: 61/255, blue: 66/255, alpha: 1.0)
-                } else {
-                    cell.tempLabel.textColor = UIColor(red: 74/255, green: 99/255, blue: 255/255, alpha: 1.0)
-                }
-                
-                //cell.tempLabel.font = UIFont.systemFontOfSize(48)
-                
-                cell.tempLabel.text = "\(predTemp)˚C"
-                cell.dateLabel.text = "í dag \n\(date)"
-                
-                returnCell = cell
-            } else {
-                let cell = tableView.dequeueReusableCellWithIdentifier("OtherDayCell", forIndexPath: indexPath) as! OtherDayCell
-                
-                if predTemp > 0 {
-                    cell.tempLabel.textColor = UIColor(red: 255/255, green: 61/255, blue: 66/255, alpha: 1.0)
-                } else {
-                    cell.tempLabel.textColor = UIColor(red: 74/255, green: 99/255, blue: 255/255, alpha: 1.0)
-                }
-                
-                cell.tempLabel.text = "\(predTemp)˚C"
-                cell.dateLabel.text = "\(weekDay) \n\(date)"
-                
-                returnCell = cell
-                
-            }
-
-        }*/
         
         return returnCell
     }
